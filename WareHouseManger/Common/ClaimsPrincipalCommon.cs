52fe6a7db6ea2abdc5fe663a6ad34aa489ae82b9
Ngô Công Minh <minhsaothe1102@gmail.com>
2021-07-19 15:49:59 +0700

    sdssss

---
+++ WareHouseManger/Common/ClaimsPrincipalCommon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace WareHouseManger.Common
+{
+    public class ClaimsPrincipalCommon
+    {
+        private ClaimsPrincipal user;
+
+        public ClaimsPrincipalCommon(ClaimsPrincipal user)
+        {
+            this.user = user;
+        }
+
+
+    }
+}
