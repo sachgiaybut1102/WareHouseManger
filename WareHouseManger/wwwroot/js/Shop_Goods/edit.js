596548631196bb04370924d5d0a8e1de09e0caf8
NGO CONG MINH <1621050237@student.humg.edu.vn>
2021-10-31 00:04:25 +0700

    fixed edit shop_goods

---
+++ WareHouseManger/wwwroot/js/Shop_Goods/edit.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    var num = $('#Price').val();
+    $('#price').val(formatNumber(num));
+
+    $('#price').keyup(function () {
+        var num = formatDisplayNumberToNumber($(this).val());
+        console.log(num);
+        $('#Price').val(num);
+    })
+});
